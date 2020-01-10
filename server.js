09025520299c4b14823ed2d615f178ac9c88a4c8
Renan <renan.azzolim@fh.com.br>
2020-01-10 20:15:59 -0300

    project basis creation

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function () {
+    console.log(`Servidor rodando na porta 3000`);
+});
+
+app.get('/', function (req, res) {
+    res.send(` 
+    <html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1> Casa do Código </h1>
+    </body> 
+        </html>`);
+});
+
+app.get('/livros', function (req, res) {
+    res.send(` 
+    <html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1> Listagem de Livros </h1>
+    </body> 
+        </html>`);
+});
+
+
+// const http = require('http');
+
+// const servidor = http.createServer(function(req, resp) {
+
+// });
+// servidor.listen(3000);
